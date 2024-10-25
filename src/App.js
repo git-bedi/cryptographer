52b82c1db6415d45d54784a198c8add36db50659
git-bedi <mailtoashishbedi@gmail.com>
2024-10-25 20:05:17 +0530

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import DecryptForm from './DecryptInputComponent';
 import EncryptForm from './EncryptInputComponent';
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h2>Cryptographer : git-bedi</h2>
       <h3>Encrypt</h3>
@@ -11,6 +13,7 @@ function App() {
       <h3>Decrypt</h3>
         <DecryptForm/>
     </div>
+    </Router>
   );
 }
 
